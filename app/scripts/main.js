c2709ef837f7bb028d531bcb4d5e6b10e7c4d1f8
Shannon <shanwes4@gmail.com>
2015-05-29 20:48:10 -0700

    catching up project, installed missing files, using bootstrapcdn, form works but needs input fields

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,10 +10,11 @@ console.log('\'Allo \'Allo!');
         entry.kitten = $("#kitten").val();
         entry.description = $("#description").val();
         var list = getList();
-        List.push(entry);
+        list.push(entry);
         saveList(list);
         displayList(getList());
         e.preventDefault();
+      console.log(list);
       }
 
       function displayList(d){
@@ -41,6 +42,8 @@ console.log('\'Allo \'Allo!');
           {"name": "Esther", "age": "2 months", "sex":"F", "Breed":"Domestic Short Hair", "imageUrl":"img/Kitten1.jpg", "description": "Esther likes long walks, playing with catnip, and be a snugglebunny"}
          ,{"name": "Garfield", "age": "3 weeks", "sex":"M", "Breed":"Orange", "imageUrl":"img/Kitten2.jpg"}
          ,{"name": "Lilly", "age": "4 months", "sex":"F", "Breed":"Persian", "imageUrl":"img/Kitten3.jpg"}
+         ,{"name": "Sam", "age": "6 months", "sex":"M", "Breed":"Whatever", "imageUrl":"img/Kitten3.jpg"}
+         ,{"name": "Dwayne", "age": "11 months", "sex":"F", "Breed":"Whatever", "imageUrl":"img/Kitten1.jpg"}
           ];
           return(defaultList);
       }
